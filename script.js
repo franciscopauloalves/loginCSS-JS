3fab8fdfc66fa506db01ab55d31b91096d0f9607
Francisco Paulo Alves <93097144+franciscopauloalves@users.noreply.github.com>
2022-05-02 20:59:30 -0300

    desafio 5

--- script.js
+++ script.js
@@ -3,24 +3,7 @@ const form = document.querySelector("form");
 
 btnLogin.addEventListener("click", event =>{
     event.preventDefault();
-
-
-const fields = [...document.querySelectorAll(".input-block input")];
-
-fields.forEach(fields => {
-    if(fields.value === "") form.classList.add("validate-error");
-});
-
-const formError = document.querySelector (".validate-error");
-if(formError){
-    formError.addEventListener("animationend", event => {
-        if (event.animationName === "nono") {
-            formError.classList.remove("validate-error");
-        }
-    });
-} else {
     form.classList.add("form-hide");
-}
 });
 
 form.addEventListener("animationstart", event => {
